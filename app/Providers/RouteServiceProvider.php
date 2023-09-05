809631ba414417b17a2d4c919da48a02e7cd71b0
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-05 16:33:14 +0900

    multiLogin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    
     /**
      * The path to the "home" route for your application.
      *
@@ -18,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     public const HOME = '/dashboard';
+    public const OWNER_HOME = '/owner/dashboard';
+    public const ADMIN_HOME = '/admin/dashboard';
 
     /**
      * The controller namespace for the application.
@@ -43,7 +46,21 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
 
-            Route::middleware('web')
+            Route::prefix('admin')
+            ->as('admin.') 
+            ->middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/admin.php'));
+
+            Route::prefix('owner')
+            ->as('owner.') 
+            ->middleware('web')
+            ->namespace($this->namespace)
+            ->group(base_path('routes/owner.php'));
+
+            Route::prefix('/')
+                ->as('user.') 
+                ->middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
