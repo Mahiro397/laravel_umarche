ca079e5bcc22e722532eb3b2b081fe9def0a4181
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-06 16:33:33 +0900

    Carbon

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Owner; //Eloquent エロクアントクアント
+use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダ
+use Carbon\Carbon;
 class OwnersController extends Controller
 {
     /**
@@ -22,7 +24,24 @@ class OwnersController extends Controller
 
     public function index()
     {
-        dd('オーナー一覧');
+        $date_now = Carbon::now();
+        $date_parse = Carbon::parse(now());
+        echo $date_now;
+        echo $date_parse;
+
+        $e_all = Owner::all();
+        $q_get = DB::table('owners')->select('name' , 'created_at')->get();
+        //$q_first = DB::table('owners')->select('name')->first();
+
+        //$c_test = collect([
+        //    'name' =>'てすと'
+        //]);
+
+        
+
+        //dd($e_all, $q_get,$q_first,$c_test);
+        return view('admin.owners.index', 
+        compact('e_all','q_get'));
     }
 
     /**
