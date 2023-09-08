cbd47c456d698db343981f674c72e71512f931ad
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-08 17:09:27 +0900

    softDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -4,47 +4,41 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Owner; //Eloquent エロクアントクアント
-use Illuminate\Support\Facades\DB; //QueryBuilder クエリビルダ
+use App\Models\Owner; // Eloquent エロクアント
+use App\Models\Shop;
+use Illuminate\Support\Facades\DB; // QueryBuilder クエリビルダ
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Validation\Rules;
-
+use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
-     public function __construct()
-     {
-         $this->middleware('auth:admin');
-        
-     }
-
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    } 
 
     public function index()
     {
-        //$date_now = Carbon::now();
-        //$date_parse = Carbon::parse(now());
-        //echo $date_now;
-        //echo $date_parse;
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
+        // echo $date_now->year;
+        // echo $date_parse;
 
-        //$e_all = Owner::all();
-        //$q_get = DB::table('owners')->select('name' , 'created_at')->get();
-        //$q_first = DB::table('owners')->select('name')->first();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
+        // $q_first = DB::table('owners')->select('name')->first();
 
-        //$c_test = collect([
-        //    'name' =>'てすと'
-        //]);
+        // $c_test = collect([
+        //     'name' => 'てすと'
+        // ]);
 
-        
+        // var_dump($q_first);
 
-        //dd($e_all, $q_get,$q_first,$c_test);
-        $owners = Owner::select('id','name' , 'email' , 'created_at')->get();
+        // dd($e_all, $q_get, $q_first, $c_test);
+        $owners = Owner::select('id', 'name', 'email', 'created_at')
+        ->paginate(3);
 
         return view('admin.owners.index', 
         compact('owners'));
@@ -70,20 +64,36 @@ class OwnersController extends Controller
     {
         //$request->name;
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:owners'],
-            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:owners',
+            'password' => 'required|string|confirmed|min:8',
         ]);
 
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
+        try{
+            DB::transaction(function () use($request) {
+                $owner = Owner::create([
+                    'name' => $request->name,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                ]);
+
+                Shop::create([
+                    'owner_id' => $owner->id,
+                    'name' => '店名を入力してください',
+                    'information' => '',
+                    'filename' => '',
+                    'is_selling' => true
+                ]);
+            }, 2);
+        }catch(Throwable $e){
+            Log::error($e);
+            throw $e;
+        }
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナー登録を実施しました');
+        ->with(['message' => 'オーナー登録を実施しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -106,6 +116,7 @@ class OwnersController extends Controller
     public function edit($id)
     {
         $owner = Owner::findOrFail($id);
+        // dd($owner);
         return view('admin.owners.edit', compact('owner'));
     }
 
@@ -126,8 +137,8 @@ class OwnersController extends Controller
 
         return redirect()
         ->route('admin.owners.index')
-        ->with('message','オーナ情報を更新しました');
-
+        ->with(['message' => 'オーナー情報を更新しました。',
+        'status' => 'info']);
     }
 
     /**
@@ -138,6 +149,21 @@ class OwnersController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Owner::findOrFail($id)->delete(); //ソフトデリート
+
+        return redirect()
+        ->route('admin.owners.index')
+        ->with(['message' => 'オーナー情報を削除しました。',
+        'status' => 'alert']);
+    }
+
+    public function expiredOwnerIndex(){
+        $expiredOwners = Owner::onlyTrashed()->get();
+        return view('admin.expired-owners', compact('expiredOwners'));
+    }
+    
+    public function expiredOwnerDestroy($id){
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+        return redirect()->route('admin.expired-owners.index'); 
     }
 }
--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,10 +1,12 @@
-@props(['status' => 'info']) 
-@php 
-if($status === 'info'){ $bgColor = 'bg-blue-300';} 
-if($status === 'error'){$bgColor = 'bg-red-500';} 
-@endphp 
-@if(session('message')) 
- <div class="{{ $bgColor }} w-1/2 mx-auto p-2 text-white"> 
- {{ session('message' )}} 
- </div> 
+@props(['status' => 'info'])
+
+@php
+if(session('status') === 'info'){$bgColor = 'bg-blue-300';}
+if(session('status') === 'alert'){$bgColor = 'bg-red-500';}
+@endphp
+
+@if(session('message'))
+  <div class="{{ $bgColor }} w-1/2 mx-auto p-2 my-4 text-white">
+    {{ session('message' )}}
+  </div>
 @endif
