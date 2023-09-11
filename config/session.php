20b0004f1cac89acc4e55d1b92f7993e4ecdcefd
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-11 10:31:17 +0900

    middleware,abort(404)

--- config/session.php
+++ config/session.php
@@ -131,6 +131,18 @@ return [
         Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
     ),
 
+
+    'cookie_owner' => env(
+        'SESSION_COOKIE',
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+    ),
+
+
+    'cookie_admin' => env(
+        'SESSION_COOKIE',
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_session'
+    ),
+
     /*
     |--------------------------------------------------------------------------
     | Session Cookie Path
