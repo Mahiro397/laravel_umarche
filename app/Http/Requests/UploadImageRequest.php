0b0ac35b8a75545d5e145d86dc73f3ecfef581be
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-18 22:35:36 +0900

    product update

--- app/Http/Requests/UploadImageRequest.php
+++ app/Http/Requests/UploadImageRequest.php
@@ -29,12 +29,12 @@ class UploadImageRequest extends FormRequest
         ];
     }
 
-    public function messages() 
-    { 
-         return [ 
-            'image' => '指定されたファイルが画像ではありません。', 
-            'mines' => '指定された拡張子（jpg/jpeg/png）ではありません。', 
-            'max' => 'ファイルサイズは2MB以内にしてください。', 
-            ]; 
-        }
+    public function messages()
+    {
+    return [
+      'image' => '指定されたファイルが画像ではありません。',
+      'mines' => '指定された拡張子（jpg/jpeg/png）ではありません。',
+      'max' => 'ファイルサイズは2MB以内にしてください。',
+      ];
+    }
 }
