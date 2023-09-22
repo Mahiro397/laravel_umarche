a55af4bdb22e73b7ca66b6c808940ed9e8ace31a
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-23 00:19:14 +0900

    stripe決済テスト

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Cart;
 use App\Models\User;
+use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
@@ -51,6 +52,73 @@ class CartController extends Controller
         Cart::where('product_id', $id)
         ->where('user_id', Auth::id())
         ->delete();
+
+        return redirect()->route('user.cart.index');
+    }
+
+    public function checkout()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
+ 
+        $lineItems = [];
+        foreach($products as $product) {
+            $quantity = '';
+            $quantity = Stock::where('product_id', $product->id)->sum('quantity');
+ 
+            if($product->pivot->quantity > $quantity){
+                return redirect()->route('user.cart.index');
+            } else {
+                $stripe_products = $stripe->products->create([
+                    'name' => $product->name,
+                    'description' => $product->information,
+                ]);
+                $stripe_price = $stripe->prices->create([
+                    'product' => $stripe_products,
+                    'unit_amount' => $product->price,
+                    'currency' => 'jpy',
+                ]);
+                $lineItem = [
+                    'price' => $stripe_price,
+                    'quantity' => $product->pivot->quantity,
+                ];
+                array_push($lineItems, $lineItem);
+            }
+        }
+        // dd($lineItems);
+        // ストライプに渡す前に在庫を減らす
+        foreach($products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['reduce'],
+                'quantity' => $product->pivot->quantity * -1,
+            ]);
+        }
+ 
+        $session = $stripe->checkout->sessions->create([
+            'line_items' => [$lineItems],
+            'mode' => 'payment',
+            'success_url' => route('user.items.index'),
+            'cancel_url' => route('user.cart.index'),
+        ]);
+ 
+        $publicKey = env('STRIPE_PUBLIC_KEY');
+ 
+        return view('user.checkout', compact('session', 'publicKey'));
+    }
+
+    public function cancel()
+    {
+        $user = User::findOrFail(Auth::id());
+
+        foreach($user->products as $product){
+            Stock::create([
+                'product_id' => $product->id,
+                'type' => \Constant::PRODUCT_LIST['add'],
+                'quantity' => $product->pivot->quantity
+            ]);
+        }
 
         return redirect()->route('user.cart.index');
     }
