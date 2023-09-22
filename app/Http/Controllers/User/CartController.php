665732bd56d64d9f8b41267ee2de98d2f3bc0170
Mahiro397 <21im0198@i-u.ac.jp>
2023-09-23 00:36:09 +0900

    決済後のカートカート

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -99,13 +99,20 @@ class CartController extends Controller
         $session = $stripe->checkout->sessions->create([
             'line_items' => [$lineItems],
             'mode' => 'payment',
-            'success_url' => route('user.items.index'),
-            'cancel_url' => route('user.cart.index'),
+            'success_url' => route('user.cart.success'),
+            'cancel_url' => route('user.cart.cancel'),
         ]);
  
         $publicKey = env('STRIPE_PUBLIC_KEY');
  
         return view('user.checkout', compact('session', 'publicKey'));
+    }
+
+    public function success()
+    {
+        Cart::where('user_id', Auth::id())->delete();
+
+        return redirect()->route('user.items.index');
     }
 
     public function cancel()
